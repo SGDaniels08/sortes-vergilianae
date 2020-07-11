e042985ff44011c8b47c2cd2238b1355a81a110a
Shawn Daniels <SGDaniels08@gmail.com>
2020-07-11 05:56:01 -0400

    Initializr'd

---
+++ src/main/java/com/sortesvergilianae/sortes/SortesVergilianaeApplication.java
@@ -0,0 +1,13 @@
+package com.sortesvergilianae.sortes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortesVergilianaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortesVergilianaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sortesvergilianae/sortes/SortesVergilianaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sortesvergilianae.sortes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortesVergilianaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
